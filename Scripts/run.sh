5324e9568c527aa5a5c83e4636e8b2a1b1e3a870
Tyler Benjamin Humpherys <tylerben@c0300ood18.ll.unc.edu>
2024-02-10 13:52:57 -0500

    New bash command to run everything on Longleaf.

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -6,17 +6,12 @@
 
 #SBATCH --mem=14G
 #SBATCH -p general
-#SBATCH -t 0-02:00:00
+#SBATCH -t 0-01:00:00
 
-make Figures/model1_heatmaps.pdf
-make Figures/model2_heatmaps.pdf
-make Figures/model3_heatmaps.pdf
-make Figures/model4_heatmaps.pdf
-make Figures/model1_survival.pdf
-make Figures/model2_survival.pdf
-make Figures/model3_survival.pdf
-make Figures/model4_survival.pdf
-make Models/model1_PLR.RData
-make Models/model2_PLR.RData
-make Models/model3_PLR.RData
-make Models/model4_PLR.RData
+make directories
+for i in {1..4}; do
+  make heatmap_figures MODEL_NUMBER=$i
+  make survival_figures MODEL_NUMBER=$i
+  make Models/model${i}_PLR.RData MODEL_NUMBER=$i
+done
+make report.pdf
